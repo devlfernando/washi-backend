6bb996e13b47907365c8a7766864f0db0e791cd7
Gustavo Novaes <gustavo.novaes182@hotmail.com>
2019-08-21 13:44:28 -0300

    Classe pessoa criada com anotações JPA, getters e setters.

---
+++ src/main/java/br/com/washi/washibackend/entity/Pessoa.java
@@ -0,0 +1,88 @@
+package br.com.washi.washibackend.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="T_PESSOA")
+@SequenceGenerator(name="pessoa", sequenceName = "SQ_T_PESSOA", allocationSize = 1)
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(generator = "pessoa", strategy = GenerationType.SEQUENCE)
+    @Column(name="PESCOD", length = 10)
+    private int codigo;
+
+    @Column(name="ENDCOD", length = 10, nullable = false)
+    private int endereco;
+
+    @Column(name="CPFCPJ", length = 12, nullable = false)
+    private String cpfcnpj;
+
+    @Column(name = "PESEML", length = 100, nullable = false)
+    private String email;
+
+    @Column(name = "PESNOM", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "PESSEN", length = 12, nullable = false)
+    private String senha;
+
+    @Column(name = "TELNUM", length = 10, nullable = false)
+    private int telefone;
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setEndereco(int endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setCpfcnpj(String cpfcnpj) {
+        this.cpfcnpj = cpfcnpj;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public int getEndereco() {
+        return endereco;
+    }
+
+    public String getCpfcnpj() {
+        return cpfcnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+}
