603a93396b5563eb6aa7ea5f7fa2ba489cff25b8
gustavonovaes <gustavo.novaes182@hotmail.com>
2019-09-21 19:30:36 -0300

    SolicitaçãoRepository e SolitacaoResource, criadas com sucesso!

---
+++ src/main/java/br/com/washi/washibackend/repository/SolicitacaoRepository.java
@@ -0,0 +1,8 @@
+package br.com.washi.washibackend.repository;
+
+import br.com.washi.washibackend.entity.Solicitacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Integer> {
+
+}
---
+++ src/main/java/br/com/washi/washibackend/resource/SolicitacaoResource.java
@@ -0,0 +1,46 @@
+package br.com.washi.washibackend.resource;
+
+import br.com.washi.washibackend.entity.Solicitacao;
+import br.com.washi.washibackend.repository.SolicitacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("solicitacao")
+public class SolicitacaoResource {
+
+    @Autowired
+    private SolicitacaoRepository rep;
+
+    @GetMapping
+    public List<Solicitacao> listar(){
+        return rep.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Solicitacao buscar(@PathVariable int id){
+        return rep.findById(id).get();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Solicitacao cadastrar(@RequestBody Solicitacao solicitacao){
+        return rep.save(solicitacao);
+    }
+
+    @PutMapping("{id}")
+    public Solicitacao atualizar(@RequestBody Solicitacao solicitacao,
+                            @PathVariable int id){
+        solicitacao.setCodigo(id);
+        return rep.save(solicitacao);
+    }
+
+    @DeleteMapping("{codigo}")
+    public void remover(@PathVariable int codigo){
+        rep.deleteById(codigo);
+    }
+}
+
